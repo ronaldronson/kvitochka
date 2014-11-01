d1952e882b506cb640f581680bc5a0152d32c9fb
RRR <rrr@Annas-Air.home>
2014-11-01 22:24:41 +0100

    master:init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+alert(location.href);
+angular.module('mobileApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
